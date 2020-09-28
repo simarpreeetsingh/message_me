ed823b1af55d745a2e06634ab2fe65addf10d7d1
Simarpreet Singh <hskzachsimba@gmail.com>
2020-09-29 00:08:06 +0530

    First commit after setting up the remote repo and working on this project through both the systems. Basic backend structure created.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,12 @@
 class SessionsController < ApplicationController
   def new
   end
+
+  def create
+    if(User.find_by(username: params[:username]))
+      render html: User.find_by(username: params[:username]).username
+    else
+      render html: 'User not found.'
+    end
+  end
 end
